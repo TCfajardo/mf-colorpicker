c9d2ac792d5dace47dd635a28593f50e6ed247a9
Catalina Fajardo <tania.fajardo01@uptc.edu.co>
2024-04-02 07:19:50 -0500

    url deploy

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3001/",
+    publicPath: "https://main--funny-frangipane-9f522c.netlify.app/",
   },
 
   resolve: {
